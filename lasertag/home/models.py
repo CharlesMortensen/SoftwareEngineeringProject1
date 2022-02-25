fdc8ce7c939ce827110c2b4a149a3f2608d40d5a
Piam Chittisane <pchittisane@gmail.com>
2022-02-25 12:49:24 -0600

    added player model and created admin

--- lasertag/home/models.py
+++ lasertag/home/models.py
@@ -9,5 +9,6 @@ class Player(models.Model):
     last_name = models.CharField(max_length=30, blank=False, null= False)
     codename = models.CharField(max_length=30, blank=False, null=False, unique=True)
 
+
     def __str__(self):
         return self.id
