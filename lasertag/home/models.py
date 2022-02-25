050cfcb53bdcf0b8af7f40464dd742aeb3c8a0ba
Piam Chittisane <pchittisane@gmail.com>
2022-02-25 12:46:40 -0600

    created player model in models and admin

--- lasertag/home/models.py
+++ lasertag/home/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    id = models.IntegerField(primary_key=True, blank=False, null=False)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null= False)
+    codename = models.CharField(max_length=30, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.id
