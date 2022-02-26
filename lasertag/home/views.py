1c5a8501dee1e2daf06365cf356611f96680080a
naedens <98190885+naedens@users.noreply.github.com>
2022-02-26 10:26:38 -0600

    added management_form data and logic to save created players

--- lasertag/home/views.py
+++ lasertag/home/views.py
@@ -10,14 +10,33 @@ def player_entry(request):
 
     RedPlayerFormSet = formset_factory(PlayerForm, extra=19)
     BluePlayerFormSet = formset_factory(PlayerForm, extra=19)
-
-    red_player_formset = RedPlayerFormSet(prefix="red")
-    blue_player_formset = BluePlayerFormSet(prefix="blue")
+    #manage_data with these params is required for each
+    manage_data_red = { 'red-TOTAL_FORMS': '20', 'red-INITIAL_FORMS': '0',}
+    manage_data_blue = { 'blue-TOTAL_FORMS': '20', 'blue-INITIAL_FORMS': '0',}
+    red_player_formset = RedPlayerFormSet(manage_data_red, prefix="red")
+    blue_player_formset = BluePlayerFormSet(manage_data_blue, prefix="blue")
 
     if request.method == 'POST':
-        post_formset = RedPlayerFormSet(request.POST)
-        for form in post_formset:
-            print(form.save())
-
+        # separate the red form data from the blue form data
+        post_data_red = {}
+        post_data_blue = {}
+        for key in request.POST:
+            if "red-" in key:
+                post_data_red[key] = (request.POST)[key]
+            elif "blue-" in key:
+                post_data_blue[key] = (request.POST)[key]
+        # make a formset for the 2 forms. must pass them the right prefix so that it knows to take it off for the save()
+        post_formset_red = RedPlayerFormSet(post_data_red, prefix="red")
+        post_formset_blue = BluePlayerFormSet(post_data_blue, prefix="blue")
+        # must validate data otherwise it won't clean it and create the cleaned_data member for save()
+        post_formset_red.is_valid()
+        post_formset_blue.is_valid()
+        # for both formsets need to loop through each form to save individual forms
+        for form in post_formset_red:
+            if form.is_valid() and form.cleaned_data: #form must be valid and non-empty before saving
+                print(form.save())
+        for form in post_formset_blue:
+            if form.is_valid() and form.cleaned_data: #form must be valid and non-empty before saving
+                print(form.save())
 
     return render(request, 'home/player_entry.html', {'title': 'Player Entry', 'red_player_formset': red_player_formset, 'blue_player_formset': blue_player_formset})
