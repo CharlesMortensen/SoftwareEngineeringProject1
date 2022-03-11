ae7b41684b63cce502f44e4953a8569e5a1eacc3
bjroden <bjroden@uark.edu>
2022-03-10 20:37:48 -0600

    Make ActivePlayer Model

--- lasertag/home/models.py
+++ lasertag/home/models.py
@@ -1,5 +1,6 @@
 from pyexpat import model
 from django.db import models
+from enum import Enum
 
 # Create your models here.
 
@@ -9,11 +10,20 @@ class Player(models.Model):
     last_name = models.CharField(max_length=30, blank=False, null= False)
     codename = models.CharField(max_length=30, blank=False, null=False, unique=True)
 
-
     def __str__(self):
         return str(self.id)
+    
+class ActivePlayer(models.Model):
+    # Team colors
+    RED = "RED"
+    BLUE = "BLUE"
+    TEAM_CHOICES = [
+        (RED, "Red"),
+        (BLUE, "Blue"),
+    ]
 
-# class ActiveGame(models.Model):
-    # id
-    # codename
-    # team
+    player_info = models.ForeignKey("Player", on_delete=models.CASCADE)
+    team = models.CharField(max_length=4, choices=TEAM_CHOICES)
+
+    def __str__(self):
+        return f"{self.player_info.id}, {self.team}"
