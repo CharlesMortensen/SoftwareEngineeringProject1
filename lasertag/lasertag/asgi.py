38035562fecdec2d3057b9f3b8bb4cab47329311
bjroden <bjroden@uark.edu>
2022-04-10 15:21:08 -0500

    Correct django setup

--- lasertag/lasertag/asgi.py
+++ lasertag/lasertag/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lasertag.settings')
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import home.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lasertag.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
