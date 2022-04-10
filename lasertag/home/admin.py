3e3ef19bc4add947a6bd87a1857edfcf513948fa
dbmid <dbmiddle@uark.edu>
2022-04-10 14:37:58 -0500

    database

--- lasertag/home/admin.py
+++ lasertag/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Player, ActivePlayer
+from .models import LaserTagMessage, Player, ActivePlayer
 # Register your models here.
 
 admin.site.register(Player)
 admin.site.register(ActivePlayer)
+admin.site.register(LaserTagMessage)
